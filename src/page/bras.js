0537aaa82a0d8c78b6950c74e6d6e792569e8553
lapthuan <lapthuan0805@gmail.com>
2024-04-16 18:51:46 +0700

    aa

--- src/page/bras.js
+++ src/page/bras.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card, Form, Input, Radio } from 'antd';
 import { Select, Space } from 'antd';
 import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
@@ -12,6 +12,8 @@ const Bras = () => {
         <TerminalOutput>'view-react-docs' will navigate to the react docs.</TerminalOutput>,
         <TerminalOutput>'clear' will clear the terminal.</TerminalOutput>,
     ]);
+
+    const [macAddress, setMacAddress] = useState('');
     const handleClick = () => {
         // Concatenate a new line to lineData
         const newLine = <TerminalOutput> $ Hello </TerminalOutput>;
@@ -19,6 +21,23 @@ const Bras = () => {
         // Alternatively, using spread operator:
         // setLineData(prevLineData => [...prevLineData, newLine]);
     }
+
+    useEffect(() => {
+        if (macAddress.length === 12) {
+            convertToMacAddress();
+        }
+    }, [macAddress]);
+
+    const handleChange = (event) => {
+        setMacAddress(event.target.value);
+    };
+
+    const convertToMacAddress = () => {
+        // Chuyển đổi chuỗi đầu vào thành định dạng địa chỉ MAC
+        const mac = macAddress.match(/.{1,2}/g).join(':').toUpperCase();
+        setMacAddress(mac);
+
+    };
     return (
         <>
             <div className='body-home'>
@@ -29,58 +48,38 @@ const Bras = () => {
                             initialValues={{
                                 size: 'small',
                             }}
-                            layout="horizontal"
+                            layout="vertical"
                             size={'small'}
                             className='form-card'
 
                         >
-                            <Form.Item label="Loại thiết bị" className='select-item'>
-                                <Select style={{ width: "100%" }} placeholder="Chọn loại thiết bị">
-                                    <Select.Option value="demo">Demoaaaaaaaaaaaaaa  </Select.Option>
-                                </Select>
-                            </Form.Item>
-                            <Form.Item label="Thiết bị" className='select-item'>
-                                <Select style={{ width: "100%" }} placeholder="Chọn thiết bị">
-                                    <Select.Option value="demo">Demoaaaaaaaaaaaaaa  </Select.Option>
-                                </Select>
-                            </Form.Item>
+                            <p>Chức năng</p>
+                            <Radio.Group name="chucnang" defaultValue={"clearUser"}>
+                                <Radio value={"clearUser"}>Clear User</Radio>
+                                <Radio value={"showUser"}>Show User</Radio>
+                                <Radio value={"showMac"}>Show Mac</Radio>
+                                <Radio value={"blockMac"}>Block Mac</Radio>
+                                <Radio value={"clearMac"}>Clear Mac</Radio>
+                            </Radio.Group>
+                            <p>Bras</p>
+                            <Radio.Group name="bras" defaultValue={"clearUser"}>
+                                <Radio value={"clearUser"}>Bras 01</Radio>
+                                <Radio value={"showUser"}>Bras 02</Radio>
+                                <Radio value={"showMac"}>Bras Bến Tre</Radio>
 
-                            <Form.Item label="Ip" className='select-item'>
-                                <Select style={{ width: "100%" }} placeholder="Chọn Ip">
-                                    <Select.Option value="demo">Demo</Select.Option>
-                                </Select>
-                            </Form.Item>
+                            </Radio.Group>
 
-                            <Form.Item label="Slot" className='select-item'>
-                                <Input placeholder='Nhập Slot' />
-                            </Form.Item>
-
-                            <Form.Item label="Port" className='select-item'>
-                                <Input placeholder='Nhập Port' />
-                            </Form.Item>
-                            <Form.Item label="Onu ID" className='select-item'>
-                                <Input placeholder='Nhập Onu ID' />
-                            </Form.Item>
-                            <Form.Item label="Vlan Net" className='select-item'>
-                                <Select style={{ width: "100%" }} placeholder="Chọn Vlan Net">
-                                    <Select.Option value="d2emo">Vlan1</Select.Option>
-                                    <Select.Option value="d233emo">Vlan12</Select.Option>
-                                    <Select.Option value="d3emo">Vlan122</Select.Option>
-                                    <Select.Option value="dem333o">Vlan1222</Select.Option>
-                                </Select>
-                            </Form.Item>
-                            <Form.Item label="Vlan Mytv" className='select-item'>
-                                <Select style={{ width: "100%" }} placeholder="Chọn Vlan Mytv">
-                                    <Select.Option value="demo">Demo</Select.Option>
-                                </Select>
-                            </Form.Item>
-
-                            <Form.Item label="Vlan IMS" className='select-item'>
-                                <Select placeholder="Chọn Vlan IMS">
-                                    <Select.Option value="demo">aaaaaaaaaa</Select.Option>
-                                </Select>
-                            </Form.Item>
                         </Form>
+                        <p>Địa chỉ Mac</p>
+                        <Space size="middle">
+                            <Input
+                                type="text"
+                                placeholder="Nhập chuỗi 12 ký tự "
+                                value={macAddress}
+                                onChange={handleChange}
+                            />
+                            <Button onClick={() => setMacAddress("")}>Clear</Button>
+                        </Space>
                     </Card>
                 </div>
 
@@ -97,40 +96,7 @@ const Bras = () => {
 
 
             </div>
-            <Card bordered={true} title="Chức năng" >
-                <Radio.Group >
-                    <Space>
-                        <Space.Compact direction="vertical">
 
-                            <Radio value={11}>Xem trạng thái Lip/Down Onu</Radio>
-                            <Radio value={21}>Xem suy hao</Radio>
-                            <Radio value={31}>Xem cấu hình Onu</Radio>
-                            <Radio value={41}>Thay Onu</Radio>
-                            <Radio value={12}>Xem Onu chưa config</Radio>
-                            <Radio value={22}>Tạo IMS GPON ALU</Radio>
-                            <Radio value={33}>Tạo Vlan 3030 ONT iGate</Radio>
-                            <Radio value={43}>Tạo Vlan 4001 ONT iGate</Radio>
-                        </Space.Compact>
-                        <Space.Compact direction="vertical">
-                            <Radio value={111}>Xem Seri nhận trên port Onu</Radio>
-                            <Radio value={211}>Xem profile Onu</Radio>
-                            <Radio value={311}>Xem Mac</Radio>
-                            <Radio value={4111}>Cấu hình Onu-Seri-VNPT</Radio>
-                            <Radio value={1333}>Set Tốc Độ</Radio>
-                            <Radio value={233}>Tìm Onu bằng Sn</Radio>
-                            <Radio value={332}>Show DHCP 3830</Radio>
-                            <Radio value={422}>Xem Vlan</Radio>
-                        </Space.Compact>
-                        <Space.Compact direction="vertical">
-                            <Button type='primary'> Run </Button>
-                        </Space.Compact>
-                    </Space>
-
-
-
-                </Radio.Group>
-
-            </Card >
         </>
 
 
